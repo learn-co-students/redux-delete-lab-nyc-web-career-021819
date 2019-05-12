1a53314b7f56ac22d86697088120510a0349585f
David Korff <dkorff12@gmail.com>
2019-05-12 18:16:59 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import BandsContainer from './components/BandsContainer'
 
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
         <BandsContainer />
-
       </div>
     );
   }
+
 };
 
 export default App;
